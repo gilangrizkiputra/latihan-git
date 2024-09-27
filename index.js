033d618de585c1ec8cbe2771cfe300d9f9d950d2
gilangrizkiputra <gilangrizkiptra@gmail.com>
2024-09-27 18:32:57 +0700

    feat: landing

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log("index")
+function landing(currentSpeed) {
+  const safeLandingSpeed = 150;
+  console.log("memulai proses pendaratan...");
+  if (currentSpeed > safeLandingSpeed) {
+    console.log("Kecepatan terlalu tinggi! Harap turunkan kecepatan.");
+  } else {
+    console.log("Kecepatan aman untuk pendaratan.");
+    console.log("Pesawat berhasil mendarat!");
+  }
+}
